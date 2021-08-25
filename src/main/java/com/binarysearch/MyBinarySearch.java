fb4e4871fedc0fc11c34e5173e7be74466891188
Gaurip10 <thegauriparhad@gmail.com>
2021-08-25 16:30:46 +0530

    Added: ability to create BST

--- src/main/java/com/binarysearch/MyBinarySearch.java
+++ src/main/java/com/binarysearch/MyBinarySearch.java
@@ -3,17 +3,56 @@ import org.junit.jupiter.api.Test;
 
 public class MyBinaryNode<K extends Comparable<K>> {
     K key;
-    MyBinaryNode<K>right;
-    MyBinaryNode<K>left;
+    MyBinaryNode<K> right;
+    MyBinaryNode<K> left;
 
-    public MyBinaryNode(K key){
-        this.key=key;
-        this.left=null;
-        this.right=null;
+    public MyBinaryNode(K key) {
+        this.key = key;
+        this.left = null;
+        this.right = null;
     }
 }
+
 public class MyBinarySearch{
     @Test
     <MyBinaryTree>
-    void
+    void given3NodeswhenaddedtoBinarySearchTree(){
+        MyBinaryTree<Integer>myBinaryNode=new MyBinaryTree<>();
+        MyBinaryTree.add(56);
+        MyBinaryTree.add(30);
+        MyBinaryTree.add(70);
+        int size =MyBinaryTree.getSize();
+        Assert.assertEquals(3,size);
+    }
 }
+public class MyBinaryTree extends Comparable<K>>{
+private MyBinaryNode<K> root;
+private MyBinaryNode<K> addRecursive;
+
+public void add(K key){
+        addRecursive=this.addRecusive(root,key()->{
+        int compareResult1=key.compareTo(root.Key);
+        });
+        this.root=addRecursive;
+        }
+private MyBinaryNode<K> addRecursive(MyBinaryNode<K> current,K key){
+        if(current==null)
+        return new MyBinaryNode<>(key);
+        int compareResult=key.compareTo(root.key);
+        if(compareResult==0)
+        return current;
+        if(compareResult< 0){
+        current.left=addRecursive(current,left,key);
+        }else{
+        current.left=addRecursive(current,right,key);
+        }
+        return current;
+        }
+        }
+
+
+
+
+
+
+
