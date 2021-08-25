74be8c2f9e423f7d1723d47be9594833baf4c3bb
Gaurip10 <thegauriparhad@gmail.com>
2021-08-25 15:51:57 +0530

    Added: Welcome to binary search tree program

---
+++ src/main/java/com/binarysearch/MyBinarySearch.java
@@ -0,0 +1,19 @@
+package com.binarysearch;
+import org.junit.jupiter.api.Test;
+
+public class MyBinaryNode<K extends Comparable<K>> {
+    K key;
+    MyBinaryNode<K>right;
+    MyBinaryNode<K>left;
+
+    public MyBinaryNode(K key){
+        this.key=key;
+        this.left=null;
+        this.right=null;
+    }
+}
+public class MyBinarySearch{
+    @Test
+    <MyBinaryTree>
+    void
+}
